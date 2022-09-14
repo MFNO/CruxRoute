22071ba641621c41cd9ffa158c0f7b2d9b420d9c
manu fourneau <manu.fourneau@inetum-realdolmen.world>
2022-09-14 16:17:00 +0200

    work on deploying aws resources + added a calendar plugin to the angular project

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+import { CreateDynamoDbStack } from "../lib/create-dynamodb-stack";
+import { CreateGatewayAndLambdaStack } from "../lib/create-gateway-and-lambda-stack";
 import { DeployAppToS3AndCloudfrontStack } from "../lib/deploy-to-s3-and-cloudfront-stack";
 
 const envCruxRoute = { account: "710911053146", region: "us-east-1" };
 
 const app = new cdk.App();
-new DeployAppToS3AndCloudfrontStack(app, "DeployAppToS3AndCloudfrontStack", {
+// new DeployAppToS3AndCloudfrontStack(app, "DeployAppToS3AndCloudfrontStack", {
+//   env: envCruxRoute,
+// });
+
+new CreateDynamoDbStack(app, "CreateDynamoDB", {
+  env: envCruxRoute,
+});
+
+new CreateGatewayAndLambdaStack(app, "CreateGateWayAndLambda", {
   env: envCruxRoute,
 });
--- crux-route/src/app/app.module.ts
+++ crux-route/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarMonthViewComponent } from './calendar-month-view/calendar-month-view.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarMonthViewComponentsModule } from './calendar-month-view/components/module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CalendarMonthViewComponent],
   imports: [
+    CommonModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    NgbModule,
+    CalendarMonthViewComponentsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
