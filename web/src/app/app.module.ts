c6ee6830173ccd4e6007eff53a7bd638b26c9daf
manu fourneau <manu.fourneau@inetum-realdolmen.world>
2022-09-16 08:55:27 +0200

    folder rename

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarMonthViewComponent } from './calendar-month-view/calendar-month-view.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarMonthViewComponentsModule } from './calendar-month-view/components/module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, CalendarMonthViewComponent],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    NgbModule,
+    CalendarMonthViewComponentsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ web/src/app/calendar-month-view/interfaces/event.ts
@@ -0,0 +1,5 @@
+export interface TrainingEvent {
+  id: string;
+  date: string;
+  description: string;
+}
